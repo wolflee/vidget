1f8a410af0fdd904534352b306f416e58d65d6c2
WolfLee <liyuan0228@gmail.com>
2013-07-02 18:18:58 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vidget::Application.config.secret_token = 'dc067131f2822e889b0b1600e8d67c482347f6c40e9337907fa19ac8040b709fd497ebff214a7f73f0e942626fc19e222cdca171b0705ff15b49972b1ef86052'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Vidget::Application.config.session_store :cookie_store, key: '_vidget_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Vidget::Application.config.session_store :active_record_store
